80c3884148e119adfac7404e4feba873d4e15a0f
Aleksander Spro <abspro@gmail.com>
2023-04-12 15:25:10 +0200

    Add some links

--- FvProject.EverquestGame.Patcher.Presentation.Client/App.xaml.cs
+++ FvProject.EverquestGame.Patcher.Presentation.Client/App.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 namespace FvProject.EverquestGame.Patcher.Presentation.Client
 {
     /// <summary>
@@ -6,5 +6,16 @@ namespace FvProject.EverquestGame.Patcher.Presentation.Client
     /// </summary>
     public partial class App : System.Windows.Application
     {
+        // https://www.google.com/search?q=wpf+application+startup+exception+handler&client=firefox-b-d&sxsrf=APwXEdfSzPFSrDY1dpfV9UqLFWV-vJM3vA%3A1681194303492&ei=P_00ZPrWHcyMxc8Px5Od2AY&ved=0ahUKEwi68ofqmKH-AhVMRvEDHcdJB2sQ4dUDCA4&uact=5&oq=wpf+application+startup+exception+handler&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIFCAAQogQ6CggAEEcQ1gQQsAM6CAghEKABEMMESgQIQRgAUMIIWOEUYPUWaAFwAXgAgAFfiAGnBZIBATmYAQCgAQHIAQjAAQE&sclient=gws-wiz-serp
+        // https://stackoverflow.com/questions/58527606/how-to-catch-an-unhandled-exception-in-c-net-wpf
+        // https://stackoverflow.com/questions/793100/globally-catch-exceptions-in-a-wpf-application
+        // https://wpf-tutorial.com/wpf-application/handling-exceptions/
+        // https://stackoverflow.com/questions/64546732/serilog-in-wpf-project
+        // https://stackoverflow.com/questions/50615719/wpf-mvvm-capture-global-exception-handler-to-context
+        // https://blog.elmah.io/logging-and-global-error-handling-in-net-7-wpf-applications/
+        // https://learn.microsoft.com/en-us/answers/questions/137974/serilog-in-wpf-project-for-vs-2019
+        // https://csharpforums.net/threads/net-core-serilog-appsettings-dependancy-injection-with-wpf.7639/
+        // https://stackoverflow.com/questions/65891760/setup-cs-does-not-setup-serilog-in-mvvmcross-wpf
+        // https://github.com/canton7/Stylet/discussions/313
     }
 }
